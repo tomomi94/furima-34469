e94b1966a107f55e976a074f71875a8b05877175
tomomi94 <kurochan1004@gmail.com>
2021-03-08 17:46:31 +0900

    Revert "Revert "Userモデルの単体テスト""
    This reverts commit 031d7afb43ae58c9b0abfc1b1f7e6d89fe22a175.

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {"たろう"}
+    email                 {Faker::Internet.free_email}
+    password              {"abc123"}
+    password_confirmation {password}
+    last_name             {"山田"}
+    first_name            {"太郎"}
+    last_name_kana        {"ヤマダ"}
+    first_name_kana       {"タロウ"}
+    birth_day             {"1993/10/4"}
+  end
+end
