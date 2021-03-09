e8e4b1a62d24b30f576ea83c25cd5c89e709a514
tomomi94 <kurochan1004@gmail.com>
2021-03-09 12:25:30 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,9 @@ class ItemsController < ApplicationController
   def index
   end
 
+  private
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  end
+
 end
