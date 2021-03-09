13da8b104af8984decf9255ad9fe16a92c12c12b
tomomi94 <kurochan1004@gmail.com>
2021-03-09 18:59:14 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,12 @@ class Item < ApplicationRecord
   has_one :purchase
   has_one_attached :image
   
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category_id
+  belongs_to :product_condition_id
+  belongs_to :estimated_shipping_id
+  belongs_to :shipping_area_id
+  belongs_to :shipping_fee_id
+
+
 end
