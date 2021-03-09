cdcc9da884383ecbd3250b703ffdbea3b5ebd45b
tomomi94 <kurochan1004@gmail.com>
2021-03-09 21:18:55 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,5 +10,15 @@ class Item < ApplicationRecord
   belongs_to :shipping_area_id
   belongs_to :shipping_fee_id
 
-
+  with_options presence: true do
+  validates :name
+  validates :content
+  validates :category_id, numericality: { other_than: 1 }
+  validates :product_condition_id, numericality: { other_than: 1 }
+  validates :estimated_shipping_id, numericality: { other_than: 1 }
+  validates :shipping_area_id, numericality: { other_than: 1 }
+  validates :shipping_fee_id, numericality: { other_than: 1 }
+  validates :price, format: { with: VALID_PRICEL_HALF }
+  validates :price, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 1000000, message: "is invalid"}
+  validates :image
 end
