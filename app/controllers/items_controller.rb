5bb8691fb7d9cb7ce84250b3bdf320f37c1e2942
tomomi94 <kurochan1004@gmail.com>
2021-03-10 18:13:26 +0900

    商品情報をテーブルに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,12 +6,21 @@ before_action:authenticate_user!, only: [:new]
   end
 
   def new
-    @items = Item.new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :content, :image, :category_id, :product_condition_id, :estimated_shipping_id, :shipping_area_id, :shipping_fee_id, :price).merge(user_id: current_user.id)
   end
 
 end
