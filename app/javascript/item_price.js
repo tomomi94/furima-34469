54dedab9a77abddd7990a3302ee2a6bbaaae954f
tomomi94 <kurochan1004@gmail.com>
2021-03-11 12:13:21 +0900

    イベント発火（金額入力）

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,7 +1,11 @@
 window.addEventListener('load', function(){
 
-const priceInput = document.getElementById("item-price");
-const salesCommission = document.getElementById("add-tax-price");
-const salesProfit = document.getElementById("profit");
-console.log(salesProfit);
+const priceInput = document.getElementById("item-price")
+const salesCommission = document.getElementById("add-tax-price")
+const salesProfit = document.getElementById("profit")
+
+priceInput.addEventListener("input", () => {
+  console.log("イベント発火");
 })
+
+});
