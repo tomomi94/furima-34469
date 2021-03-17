c47d94ccf196e317b412adfd31972ef9a852633b
tomomi94 <kurochan1004@gmail.com>
2021-03-17 12:23:05 +0900

    フォーム情報の保存

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,10 +2,23 @@ class PurchasesController < ApplicationController
 
   def index
     @shipping_address_purchas = ShippingAddressPurchas.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
+    @item = Item.find(params[:item_id])
+    @shipping_address_purchas = ShippingAddressPurchas.new(purchas_params)
+    if @shipping_address_purchas.valid?
+      @shipping_address_purchas.save
+      redirect_to root_path
+    else
+      render :index
+    end
   end
 
-end
+  private
 
+  def purchas_params
+    params.require(:shipping_address_purchas).permit(:post_code, :shipping_area_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
+end
--- app/models/shipping_address_purchas.rb
+++ app/models/shipping_address_purchas.rb
@@ -1,20 +1,19 @@
 class ShippingAddressPurchas
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_code, :shipping_area_id, :municipality, :address, :building_name, :phone_number, :purchase
+  attr_accessor :user_id, :item_id, :post_code, :shipping_area_id, :municipality, :address, :building_name, :phone_number
 
   with_options presence: true do
-   validates :user_id
-   validates :item_id
-   validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-   validates :shipping_area_id, numericality: { other_than: 1 }
-   validates :municipality
-   validates :address
-   validates :phone_number, format: { with: /^0\d{,10}$/ }
-   validates :purchase_id
+    validates :user_id
+    validates :item_id
+    validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :shipping_area_id, numericality: { other_than: 1 }
+    validates :municipality
+    validates :address
+    validates :phone_number, format: { with: /\A\d{11}\z/ }
   end
 
   def save
-    shipping_address = ShippingAddress.create(item_id: item_id, user_id: user_id)
-    purchas = Purchas.create(post_code: post_code, shipping_area_id: shipping_area_id, municipality: municipality, address: address, phone_number: phone_number, purchase_id: purchase_id)
+    purchase = Purchase.create(item_id: item_id, user_id: user_id)
+    shipping_address = ShippingAddress.create(post_code: post_code, shipping_area_id: shipping_area_id, municipality: municipality, address: address, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
