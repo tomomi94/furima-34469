959a784c2614756cf974338fc9d1dcf65b432e3c
tomomi94 <kurochan1004@gmail.com>
2021-03-18 18:19:33 +0900

    商品購入機能_修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -51,6 +51,6 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path if current_user.id == @item.user_id || @item.purchase.present?
+    redirect_to root_path if current_user.id != @item.user_id || @item.purchase != nil
   end
 end
--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -40,7 +40,7 @@ class PurchasesController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path unless current_user.id == @item.user_id && @item.purchase.nil?
+    redirect_to root_path if current_user.id == @item.user_id || @item.purchase != nil
   end
 
 end
