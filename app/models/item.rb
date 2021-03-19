38175a0f94298d4767f4660d33870212edfb636b
tomomi94 <kurochan1004@gmail.com>
2021-03-19 11:25:48 +0900

    バリデーション_修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,7 +15,7 @@ class Item < ApplicationRecord
     validates :content
     validates :price, format: { with: /\A[0-9]+\z/ }
     validates :price, presence: true,
-                      numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 1_000_000, message: 'is invalid' }
+                      numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than: 10_000_000 }
     validates :image
     with_options numericality: { other_than: 1 } do
       validates :category_id
